152a66236a12105995d0c38b7f01bdf2832830aa
Thrisith <tthrisith@gmail.com>
2021-12-01 20:12:47 +0530

    3nd commit

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   };
   const logoutHandler = () => {
     localStorage.removeItem("isLoggedIn");
-    setIsLoggedIn(fals);
+    setIsLoggedIn(false);
   };
   return (
     <AuthContext.Provider
